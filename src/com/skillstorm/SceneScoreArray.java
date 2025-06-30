ad08baf74281c2ba856a884470cc6732309dc424
egithublizabeth <eyang1512@gmail.com>
2025-06-30 17:33:53 -0400

    renamed file

--- src/com/skillstorm/SceneScoreArray.java
+++ src/com/skillstorm/SceneScoreArray.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class SceneScoreArrayTest {
+public class SceneScoreArray {
 
 	public static void main(String[] args) 
 {
